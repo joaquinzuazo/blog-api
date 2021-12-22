9443364a57ac57ce7bf084d684f8f3033eee40cb
Joaquin Zuazo Ciolfi <jzuazociolfi@gmail.com>
2021-12-22 16:18:02 -0300

    docs API

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'rest_framework',
+    'drf_yasg',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
